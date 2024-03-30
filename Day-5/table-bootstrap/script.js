bd2bf68009b53928c57801fb7d14eb3df8fe2637
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-03-30 15:23:12 +0530

    vars:js

--- Day-5/table-bootstrap/script.js
+++ Day-5/table-bootstrap/script.js
@@ -1,10 +1,34 @@
 let a;
-console.log(typeof a); // undefined
+console.log("Value of a is: ",a);
+console.log("Data type of a is: ",typeof a); // undefined
 a=10;
-console.log(typeof a); // 10
+console.log("Value of a is: ",a);
+console.log("Data type of a is: ",typeof a); // 10
 a="Hello";
-console.log(typeof a); // Hello
+console.log("Value of a is: ",a);
+console.log("Data type of a is: ",typeof a); // Hello
 a=true;
-console.log(typeof a); // true
+console.log("Value of a is: ",a);
+console.log("Data type of a is: ",typeof a); // true
 a=null;
-console.log(typeof a); // null
+console.log("Value of a is: ",a);
+console.log("Data type of a is: ",typeof a); // null
+
+//Data types and Fucntionns?
+function fun(){
+    console.log("Function is called");
+}
+fun();
+//array?
+let arr=[1,2,3,4,5];
+console.log(arr);
+console.log(arr[0]);
+console.log(typeof arr);
+// objects?
+let obj={
+    name: "Abc",
+    age: 11
+}
+console.log(obj);
+console.log(typeof obj);
+
