7351dcb964e3e8cfcaeaa60f3935b02dc9efe102
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-03-30 15:12:38 +0530

    add:vars

---
+++ Day-5/table-bootstrap/script.js
@@ -0,0 +1,10 @@
+let a;
+console.log(typeof a); // undefined
+a=10;
+console.log(typeof a); // 10
+a="Hello";
+console.log(typeof a); // Hello
+a=true;
+console.log(typeof a); // true
+a=null;
+console.log(typeof a); // null
