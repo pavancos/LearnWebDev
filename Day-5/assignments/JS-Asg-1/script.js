bcf52cbe8ceaa705cba31004e01117a4f3c07920
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-04 20:45:42 +0530

    rearr

--- Day-5/assignments/JS-Asg-1/script.js
+++ Day-5/assignments/JS-Asg-1/script.js
@@ -1,28 +1,28 @@
-let a=7;
-let b=9;
-//1-Arithmentic Operations
-console.log("Sum: "+(a+b))
-console.log("Difference: "+(a-b))
-console.log("Product: "+(a*b))
-console.log("Quotient: "+a/b)
-console.log("Remainder: "+a%b)
-
-console.log("Unary Increment & Decrement Operators")
-console.log("Increment b: "+(++b));
-console.log("Decrement b: "+(--b));
-
-console.log("Comparison Operators")
-console.log("a > b ?: "+(a>b));
-console.log("a < b ?: "+(a<b));
-console.log("a >= b ?: "+(a>=b));
-console.log("a <= b ?: "+(a<=b));
-console.log("a == b ?: "+(a==b));
-console.log("a != b ?: "+(a!=b));
-
-console.log("Difference between == and === operators")
-let first=10;
-let sec="10";
-console.log("first == sec ?: "+(first==sec));
-console.log("first === sec ?: "+(first===sec));
-console.log("=== is for strict comparisons")
-console.log("== is for just value comaprisons")
+let a = 7;
+let b = 9;
+// 1. Write a program to implement Arithmetic operations
+console.log("Sum: " + (a + b));
+console.log("Difference: " + (a - b));
+console.log("Product: " + a * b);
+console.log("Quotient: " + a / b);
+console.log("Remainder: " + (a % b));
+// 2. Write a program to test unary increment & decrement operators
+console.log("Unary Increment & Decrement ");
+console.log("Increment b: " + ++b);
+console.log("Decrement b: " + --b);
+// 3. Write a program to verify comparison operators
+console.log("Comparison Operators");
+console.log("a > b ?: " + (a > b));
+console.log("a < b ?: " + (a < b));
+console.log("a >= b ?: " + (a >= b));
+console.log("a <= b ?: " + (a <= b));
+console.log("a == b ?: " + (a == b));
+console.log("a != b ?: " + (a != b));
+// 4. Write a program to find difference between == and === operators
+console.log(" == and === operators");
+let first = 10;
+let sec = "10";
+console.log("first == sec ?: " + (first == sec));
+console.log("first === sec ?: " + (first === sec));
+console.log("=== is for strict comparisons");
+console.log("== is for just value comaprisons");
