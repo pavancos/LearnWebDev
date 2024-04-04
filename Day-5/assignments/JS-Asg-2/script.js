eccef6474c6725e7c505b5d6a15b32b9f724f17b
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-04 21:31:23 +0530

    fin:js2

--- Day-5/assignments/JS-Asg-2/script.js
+++ Day-5/assignments/JS-Asg-2/script.js
@@ -1,35 +1,26 @@
-let a=9;
-let b=10;
-let c=20;
-console.log("The value of a is: "+a);
-console.log("The value of b is: "+b);
-console.log("The value of c is: "+c);
+let a=7;
+let b=18;
+let c=25;
+console.log(a+" "+b+" "+c)
+console.log("Greatestof two: ", a>b?a:b);
+console.log("Greatestof three: ", a>b?(a>c?a:c):b>c?b:c);
 
-console.log("Greatest number in two numbers: ", a>b?a:b);
-console.log("Greatest number in three numbers: ", a>b?(a>c?a:c):b>c?b:c);
-
-let n=10;
-console.log("The value of n is: "+n);
-console.log("Factors Of " +n+ " are:")
-for(let i=1;i<=n;i++){
-    if(n%i==0){
+let n=18;
+console.log(n);
+console.log("Factors Of " +n+ " :")
+for(let i=1;i<=n;i++)
+    if(n%i==0)
         console.log(i);
-    }
-}
 
-console.log("Check whether the number is prime or not");
-let count=0;
-for(let i=1;i<=n;i++){
-    if(n%i==0){
-        count++;
-    }
-}
-if(count==2)
-    console.log("The number is prime");
-else
-    console.log("The number is not prime");
 
-console.log("Print even factors of a number");
+console.log("Prime or Not");
+let cnt=0;
+for(let i=1;i<=n;i++)
+    if(n%i==0)
+        cnt++;
+(cnt==2)?console.log(n+" is prime"):console.log(n+" is not prime");
+
+
 console.log("Even factors of " +n+ " are:");
 for(let i=1;i<=n;i++){
     if(n%i==0 && i%2==0){
@@ -37,12 +28,12 @@ for(let i=1;i<=n;i++){
     }
 }
 
-console.log("Sum of Digits in a Number");
-let num=1234;
-console.log("The number is: "+num);
-let sum=0;
+console.log("Sum of Digits");
+let numb=1234;
+console.log("The number is: "+numb);
+let s=0;
 for(let i=0;i<n;i++){
-    sum+=num%10;
-    num=Math.floor(num/10);
+    s+=numb%10;
+    numb=Math.floor(numb/10);
 }
-console.log("The sum of digits is: "+sum);
+console.log("The sum of digits is: "+s);
