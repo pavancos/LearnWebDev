3f3d33614f183bdcf0a78289d102de122c0ba1fb
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-04 20:43:52 +0530

    add:js1

---
+++ Day-5/assignments/JS-Asg-1/script.js
@@ -0,0 +1,28 @@
+let a=7;
+let b=9;
+//1-Arithmentic Operations
+console.log("Sum: "+(a+b))
+console.log("Difference: "+(a-b))
+console.log("Product: "+(a*b))
+console.log("Quotient: "+a/b)
+console.log("Remainder: "+a%b)
+
+console.log("Unary Increment & Decrement Operators")
+console.log("Increment b: "+(++b));
+console.log("Decrement b: "+(--b));
+
+console.log("Comparison Operators")
+console.log("a > b ?: "+(a>b));
+console.log("a < b ?: "+(a<b));
+console.log("a >= b ?: "+(a>=b));
+console.log("a <= b ?: "+(a<=b));
+console.log("a == b ?: "+(a==b));
+console.log("a != b ?: "+(a!=b));
+
+console.log("Difference between == and === operators")
+let first=10;
+let sec="10";
+console.log("first == sec ?: "+(first==sec));
+console.log("first === sec ?: "+(first===sec));
+console.log("=== is for strict comparisons")
+console.log("== is for just value comaprisons")
