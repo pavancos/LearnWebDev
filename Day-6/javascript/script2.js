e1648867266899b12568b81e1e006e9d93fffdb0
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-06 14:07:41 +0530

    add:sess2

---
+++ Day-6/javascript/script2.js
@@ -0,0 +1,22 @@
+//filter
+let arr=[1,2,3,4,5,6,7,8,9];
+let even=arr.filter(function(item){
+    return item%2==0;
+});
+
+//without filter
+let numbers=[10,20,30,40,50,60,70,80,90];
+let gr=[];
+for(let e of numbers){
+    if(e>50)
+        gr.push(e);
+}
+console.log(numbers);
+console.log(gr);
+let g=numbers.filter(function(item){
+    return item>50;
+});
+console.log(gr);
+
+let G=numbers.filter((e)=>e>50);
+console.log(G);
