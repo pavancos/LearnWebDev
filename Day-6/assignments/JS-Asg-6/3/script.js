6297acebd94df4c8fe8dce07f3fe06474fba6e33
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-06 20:51:54 +0530

    fin:as6

--- Day-6/assignments/JS-Asg-6/3/script.js
+++ Day-6/assignments/JS-Asg-6/3/script.js
@@ -1,22 +1,26 @@
-class Product {
-    constructor(prodNum, prodName, model, price) {
-        this.prodNum = prodNum;
-        this.prodName = prodName;
-        this.model = model;
-        this.price = price;
-    }
-    getFinPrice(disc) {
-        return this.price - (this.price * (disc / 100));
-    }
-}
-const product1 = new Product("H80P", "mobile", "M78JK10", 10000);
-const product2 = new Product("A12Q", "pc", "X8R345", 15000);
-const product3 = new Product("C56Z", "books", "Y2T789", 20000);
-const product4 = new Product("E34R", "bike", "Z5U234", 12000);
-const product5 = new Product("G78S", "car", "P9O876", 18000);
+const students = [
+    { name: "ravi", marks: { maths: 89, physics: 70, chemistry: 88 } },
+    { name: "bhanu", marks: { maths: 98, physics: 50, chemistry: 68 } },
+    { name: "kiran", marks: { maths: 50, physics: 82, chemistry: 94 } }
+];
 
-console.log(product1.prodName, "Discounted Price:", product1.getFinPrice(25));
-console.log(product2.prodName, "Discounted Price:", product2.getFinPrice(15));
-console.log(product3.prodName, "Discounted Price:", product3.getFinPrice(20));
-console.log(product4.prodName, "Discounted Price:", product4.getFinPrice(30));
-console.log(product5.prodName, "Discounted Price:", product5.getFinPrice(10));
+const findH = (sub) => {
+    const hiStud = students.reduce((hi, stu) => {
+        const marks = stu.marks[sub];
+        if (marks > hi.hiM)
+            return { hiM: marks, names: [stu.name] };
+        else if (marks === hi.hiM)
+            hi.names.push(stu.name);
+        return hi;
+    }, { hiM: 0, names: [] }).names;
+    
+    return hiStud;
+};
+
+const highM = findH('maths');
+const highP = findH('physics');
+const highC = findH('chemistry');
+
+console.log("Highest in Maths:", highM);
+console.log("Highest in Physics:", highP);
+console.log("Highest in Chemistry:", highC);
