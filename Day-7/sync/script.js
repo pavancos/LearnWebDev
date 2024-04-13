5a51010d2bed769fdb2b234e649be12f68f08273
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-13 14:38:25 +0530

    add:sync

---
+++ Day-7/sync/script.js
@@ -0,0 +1,14 @@
+console.log('Start');
+
+// for(let i = 0; i < 1e9; i++) {
+//     // This loop will take a significant amount of time to complete
+// }
+
+
+setTimeout(
+    () => console.log('Timeout 1'), 0
+);
+
+
+console.log('End');
+
