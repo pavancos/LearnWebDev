d57f4f8d5703108930b51edef753c25141662742
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-14 01:22:26 +0530

    fin:asg91

--- Day-7/assignments/JS-Asg-9/1/script.js
+++ Day-7/assignments/JS-Asg-9/1/script.js
@@ -1,1 +1,22 @@
 // 1. Create a promise which can be either fulfilled or rejected after 5 secs
+let Delay = false;
+setTimeout(() => {
+  Delay = true;
+}, 5000);
+const promiseFiveSec = new Promise((fulfil, reject) => {
+  setTimeout(() => {
+    if (Delay) {
+      fulfil("Promise Fulfiled");
+    } else {
+      reject("Promise rejected");
+    }
+  }, 5003);
+});
+console.log(promiseFiveSec);
+promiseFiveSec
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
