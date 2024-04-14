55e9387edf6082971f7da271309d38f2fb66ab4e
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-14 16:45:27 +0530

    mrg:8to1

--- Day-7/assignments/JS-Asg-8/1/script.js
+++ Day-7/assignments/JS-Asg-8/1/script.js
@@ -3,3 +3,14 @@ import employees from "./employee.js";
 let hydEmp=employees.filter(emp=>emp.address.city.toLowerCase()==="hyderabad");
 let hydEmpNames=hydEmp.map(emp=>emp.name);
 console.log("Employees in Hyderabad: ",hydEmpNames);
+//2. Find the name and address of employees whose age is between 40 and 50
+let oldEmp=employees.filter(emp=>(emp.age>=40&&emp.age<=50));
+let oldEmpNames=oldEmp.map(emp=>({name:emp.name, address:emp.address}));
+console.log("Employees between 40 and 50 years: ",oldEmpNames);
+//3. Find the employees who are not from Hyderabad
+let nonHydEmp=employees.filter(emp=>emp.address.city.toLowerCase()!=="hyderabad");
+console.log("Employees not in Hyderabad: ",nonHydEmp);
+//4. Find the names (array of names) of employees whose skill is "reactjs"
+let reactEmp=employees.filter(emp=>emp.skills.includes('reactjs'));
+let reactEmpNames=reactEmp.map(emp=>emp.name);
+console.log("Employees with skill ReactJs:", reactEmpNames);
