a35550be3f08c751a197882963fec82654413c99
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-05-31 18:00:29 +0530

    chore: Refactor script.js for improved code organization and readability

--- Day-8/assignments/json/script.js
+++ Day-8/assignments/json/script.js
@@ -1,23 +1,24 @@
 async function getPosts(){
     const res = await fetch('https://jsonplaceholder.typicode.com/todos');
     const data = await res.json();
+    let Tab = document.querySelector('table');
+
     for(let i =0;i<data.length;i++){
-        let Tab = document.querySelector('table');
         let tr = document.createElement('tr');
         let td = document.createElement('td');
         for(let j=0;j<4;j++){
             td = document.createElement('td');
             if(j==0){
-                td.innerHTML = data[i].userId;
+                td.textContent = data[i].userId;
             }
             else if(j==1){
-                td.innerHTML = data[i].id;
+                td.textContent = data[i].id;
             }
             else if(j==2){
-                td.innerHTML = data[i].title;
+                td.textContent = data[i].title;
             }
             else{
-                td.innerHTML = data[i].completed;
+                td.textContent = data[i].completed;
             }
             tr.appendChild(td);
         }
