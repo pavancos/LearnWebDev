1d5b972ae559d95795c130e92da88ee25c945d8c
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-05-31 18:30:42 +0530

    chore: Update HTML and JavaScript files for JSON assignment

--- Day-8/assignments/json/script.js
+++ Day-8/assignments/json/script.js
@@ -1,23 +1,44 @@
-async function getPosts(){
+async function getPosts() {
     const res = await fetch('https://jsonplaceholder.typicode.com/todos');
     const data = await res.json();
     let Tab = document.querySelector('table');
+    let table = document.querySelector('table tbody');
 
-    for(let i =0;i<data.length;i++){
+
+    let grid = document.querySelector('#Grid');
+    data.forEach(element => {
+        let card = document.createElement('div');
+        card.classList.add('card');
+        let p = document.createElement('p');
+        p.textContent = element.userId;
+        card.appendChild(p);
+        p = document.createElement('p');
+        p.textContent = element.id;
+        card.appendChild(p);
+        p = document.createElement('h3');
+        p.textContent = element.title;
+        card.appendChild(p);
+        p = document.createElement('p');
+        p.textContent = element.completed;
+        card.appendChild(p);
+        grid.appendChild(card);
+    });
+
+    for (let i = 0; i < data.length; i++) {
         let tr = document.createElement('tr');
         let td = document.createElement('td');
-        for(let j=0;j<4;j++){
+        for (let j = 0; j < 4; j++) {
             td = document.createElement('td');
-            if(j==0){
+            if (j == 0) {
                 td.textContent = data[i].userId;
             }
-            else if(j==1){
+            else if (j == 1) {
                 td.textContent = data[i].id;
             }
-            else if(j==2){
+            else if (j == 2) {
                 td.textContent = data[i].title;
             }
-            else{
+            else {
                 td.textContent = data[i].completed;
             }
             tr.appendChild(td);
@@ -25,6 +46,5 @@ async function getPosts(){
         Tab.appendChild(tr);
     }
 }
-
 
 getPosts();
