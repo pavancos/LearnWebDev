0dd54a95d22a1607d7f788be5dadd237c0bc3fdd
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-06-02 00:58:27 +0530

    Refactor component structure and improve organization and readability

--- Day-9/assignments/users/src/components/user/User.jsx
+++ Day-9/assignments/users/src/components/user/User.jsx
@@ -4,10 +4,25 @@ import './User.css'
 const User = () => {
     //8 DC Superhero data
     const users = [
-        { id: 1, name: 'Superman', realName: 'Clark Kent', power: 'Super strength', dp:'./superman.jpg' },
-        { id: 2, name: 'Batman', realName: 'Bruce Wayne', power: 'Genius-level intellect', dp:'./batmanSquare.jpg' },
-        { id: 3, name: 'Wonder Woman', realName: 'Diana Prince', power: 'Super strength', dp:'./wonderwoman.jpg' },
-
+      {
+        title:"News App using ReactJS (Parsing XML RSS feed to JSON)",
+        desc:"News App using ReactJS (Parsing XML RSS feed to JSON) Photo by Christina Morillo from PexelsLive Demo:",
+        auth:"Mehul Kothari",
+        date:"Aug 12, 2020 4:32",
+        dp:"./first.webp"
+      },{
+        title:"Medium like Blogging App Using Angular 9 and Firebase",
+        desc:"Creating Medium like Blogging App Using Angular 9 and Firebase Photo by Miguei A.Padrinian from PexelsLive Demo:",
+        auth:"Mehul Kothari",
+        date:"Jul 10, 2020 10:59",
+        dp:"./second.webp"
+      },{
+        title:"Covid 19 Tracker (Statistics) app Using Angular",
+        desc:"Covid 19 Tracker (Statistics) app Using Angular Dark Mode Demonstration: Everyone is aware",
+        auth:"Mehul Kothari",
+        date:"Apr 04, 2020 08:46",
+        dp:"./third.png"
+      },
     ];
   return (
     <main>
--- Day-9/assignments/users/src/components/userlist/Userlist.jsx
+++ Day-9/assignments/users/src/components/userlist/Userlist.jsx
@@ -3,12 +3,21 @@ import './Userlist.css'
 const Userlist = (props) => {
   return (
     <section className='card'>
-        <figure className='picHold'>
-            <img className='UserPic' src={props.user.dp} alt="Profile" />
-        </figure>
-        <h2>{props.user.name}</h2>
-        <p>Real name: {props.user.realName}</p>
-        <p>Power: {props.user.power}</p>
+      <figure className='picHold'>
+        <img className='UserPic' src={props.user.dp} alt="Profile" />
+
+          <img src="./look.jpg" alt="" className="autIc" />
+
+      </figure>
+      <div className="info">
+        <h2>{props.user.title}</h2>
+        <p>{props.user.desc}</p>
+        <div className='meta'>
+          <p><img src="./author.png" alt="" className="icon" />{props.user.auth}</p>
+          <p><img src="./calendar.png" alt="" className="icon" />{props.user.date}</p>
+        </div>
+      </div>
+
     </section>
   )
 }
