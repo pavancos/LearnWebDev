fa89a0adab3eea34dfa14aca1ce289e2433b29ed
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-06-01 12:05:51 +0530

    feat: Add state for person name and implement changeName function in App.jsx

--- Day-9/states/src/App.jsx
+++ Day-9/states/src/App.jsx
@@ -11,13 +11,21 @@ function App() {
   const res = () => {
     setCount(0);
   }
+  const[person, setPerson] = useState({ name: 'pavan', age: 19, pid: 1234 });
+  const changeName = () => {
+    setPerson({ ...person, name: 'vignesh' });
+  }
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={inc}>INCREMENT</button>
       <button onClick={dec}>DECREMENT</button>
       <button onClick={res}>RESET</button>
-      
+      <br />
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.pid}</h2>
+      <button onClick={changeName}>CHANGE NAME</button>
     </div>
   )
 }
