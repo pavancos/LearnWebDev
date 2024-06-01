9f7b064c7d4a32b1fae622fcbde8821ad10e5f96
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-06-01 15:15:01 +0530

    refactor: Improve component organization and readability

--- Day-9/intoComponents/components/test1/Test1.jsx
+++ Day-9/intoComponents/components/test1/Test1.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Test2 from '../test2/Test2'
-const Test1 = () => {
+const Test1 = (props) => {
   return (
     <div>
-      <h1>Test1 Component</h1>
+      <h1>Props passes:</h1>
+      <h2>{props.x}</h2>
       <Test2/>
     </div>
   )
--- Day-9/intoComponents/src/App.jsx
+++ Day-9/intoComponents/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
 import './App.css'
-import Test1 from './components/test1/Test1'
+import Test1 from './components/test1/Test1';
+
 function App() {
+  let us1="Pavan"
+  let us2="Vignesh"
+
   return (
     <div>
       <h1>App Component</h1>
-      <Test1/>
+      <Test1 x={us1}/>
+      <Test1 x={us2}/>
 
     </div>
   )
