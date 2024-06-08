081dd314083704f4c0e38b10741ddd29cdd9c69a
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-06-08 15:33:27 +0530

    chore: Refactor Parent component and remove unused code

--- Day-11/forms/src/components/parent/Parent.jsx
+++ Day-11/forms/src/components/parent/Parent.jsx
@@ -1,16 +1,11 @@
 import React,{useState} from 'react'
 import Child from '../child/Child'
-
-
-
 const Parent = () => {
     let [counter, setCounter] = useState(0)
     let [sample, setSample] = useState(100)
-
     function getDataFromChild(data){
         setSample(data)
     }
-
     return (
         <div className='bg-dark max-auto mt-5 p-4 text-light text-center '>
             <h1>Parent</h1>
