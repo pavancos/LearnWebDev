a6972365343ab55f3e6a5ef9e7fff33cfb7c2df5
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-06-15 11:31:57 +0530

    chore: Update npm dependencies to include react-icons

--- Day-13/simple-ecomm/src/App.jsx
+++ Day-13/simple-ecomm/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { FaHome } from "react-icons/fa";
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Hello</h1>
+      <FaHome  className=''/>
     </>
   )
 }
