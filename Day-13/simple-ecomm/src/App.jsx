570f0674fdc666715912e02101ffd40277eaa3a0
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-06-15 12:06:00 +0530

    chore: Refactor App component to use react-router-dom for routing

--- Day-13/simple-ecomm/src/App.jsx
+++ Day-13/simple-ecomm/src/App.jsx
@@ -1,13 +1,45 @@
 import { useState } from 'react'
 import { FaHome } from "react-icons/fa";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RootLayout from './RootLayout';
+import Home from './components/home/Home';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
+import AboutUs from './components/aboutus/AboutUs';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 
 function App() {
+  const browserRouter = createBrowserRouter([
+    {
+      path: '',
+      element: <RootLayout />,
+      children: [
+        {
+          path: '/',
+          element: <Home />
+        },
+        {
+          path: '/register',
+          element: <Register />
+        },
+        {
+          path: '/login',
+          element: <Login />
+        },
+        {
+          path: '/aboutus',
+          element: <AboutUs />
+        }
+      ]
+    }
+  ])
 
   return (
     <>
-      
+      <div className="main">
+        <RouterProvider></RouterProvider>
+      </div>
     </>
   )
 }
---
+++ Day-13/simple-ecomm/src/RootLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
+import { Outlet } from 'react-router-dom'
+const RootLayout = () => {
+    return (
+        <div>
+            <Header />
+            
+            <div style={{minHeight:'100vh'}}>
+                <Outlet />
+            </div>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default RootLayout
