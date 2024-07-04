2940093ac50585bbc34c3e2796f61a3c4124fc78
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-07-04 20:15:59 +0530

    added links

---
+++ Day-15/assignments/N1/onClick.js
@@ -0,0 +1,11 @@
+let head=document.querySelectorAll('h4');
+console.log(head)
+head[0].addEventListener('click',function(){
+    window.open('https://github.com/pavancos/LearnWebDev/blob/main/Day-15/assignments/N1/first.js');
+})
+head[1].addEventListener('click',function(){
+    window.open('https://github.com/pavancos/LearnWebDev/blob/main/Day-15/assignments/N1/second.js');
+})
+head[2].addEventListener('click',function(){
+    window.open('https://github.com/pavancos/LearnWebDev/blob/main/Day-15/assignments/N1/script.js');
+})
