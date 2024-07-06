2507f2b8215c222c8d0df9d56fae8de37de0d2e6
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-07-06 09:06:57 +0530

    add:cpy

---
+++ Day-15 copy/nodejs/core-modules/second.js
@@ -0,0 +1,4 @@
+let data = require('./first'); // importing the person object from first.js
+console.log('imported: ', data);
+console.log(data.person.name); // abc
+console.log(data.marks); // [ 10, 20, 30, 40, 50 ]
