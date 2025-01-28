e5f127a6fb2f02fb9fa5d990390f77bbba127779
pavancos <pavankc005@gmail.com>
2025-01-28 09:57:31 +0530

    feat: http-client server

---
+++ mernlab/labasgs/3week/http-client/server/index.js
@@ -0,0 +1,16 @@
+import http from 'http';
+const server = http.createServer((req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  if (req.url === '/') {
+    res.end('/ endpoint')
+  } else {
+    res.end('404')
+  }
+});
+let port = 5000
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`)
+});
